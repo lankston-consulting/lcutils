68adb12b2abcc4abb83e9724ed8b9526f0a42255
cg117634 <colton11gerth@gmail.com>
2023-01-27 13:59:33 -0600

    added upload_blob function

--- src/lcutils/gcs.py
+++ src/lcutils/gcs.py
@@ -180,6 +180,21 @@ class GcsTools(object):
         blob.upload_from_file(source_file_obj, rewind=True)
         # We return the blob object in order to make the temporary file public for download in main.py
         return blob
+        
+    @staticmethod
+    def upload_blob(bucket_name, source_file_name, destination_blob_name):
+        """Uploads a file to the bucket."""
+        # The ID of your GCS bucket
+        # bucket_name = "your-bucket-name"
+        # The path to your file to upload
+        # source_file_name = "local/path/to/file"
+        # The ID of your GCS object
+        # destination_blob_name = "storage-object-name"
+
+        bucket = GcsTools._client.bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
+
+        blob.upload_from_filename(source_file_name)
 
     @staticmethod
     def upload_from_memory(
